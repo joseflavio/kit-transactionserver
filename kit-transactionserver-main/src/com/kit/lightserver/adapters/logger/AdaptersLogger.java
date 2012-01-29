7b4ab12512e6494a9aa6e84bbd2c7ca505a04357
Jose Flavio A <joseflavio at gmail dot com>
2012-01-29 23:22:41 +0100

    [dev] improving the log of primitives, adding meaningful names

--- kit-transactionserver-main/src/com/kit/lightserver/adapters/logger/AdaptersLogger.java
+++ kit-transactionserver-main/src/com/kit/lightserver/adapters/logger/AdaptersLogger.java
@@ -10,11 +10,13 @@ public final class AdaptersLogger {
     static private final Logger LOGGER = LoggerFactory.getLogger(AdaptersLogger.class);
 
     static public void logSending(final Primitive primitive) {
-        LOGGER.trace("[SEND] primitive=" + primitive);
+        final String name = PrimitiveNameTranslator.getName(primitive);
+        LOGGER.trace("[SEND] name="+name+", primitive=" + primitive);
     }
 
     static public void logReceived(final Primitive primitive) {
-        LOGGER.trace("[RECV] primitive=" + primitive);
+        final String name = PrimitiveNameTranslator.getName(primitive);
+        LOGGER.trace("[RECV] name="+name+", primitive=" + primitive);
     }
 
 }// class
---
+++ kit-transactionserver-main/src/com/kit/lightserver/adapters/logger/PrimitiveNameTranslator.java
@@ -0,0 +1,102 @@
+package com.kit.lightserver.adapters.logger;
+
+import kit.primitives.authentication.AuthenticationRequest;
+import kit.primitives.authentication.AuthenticationResponse;
+import kit.primitives.base.Primitive;
+import kit.primitives.forms.FormOperation;
+
+final class PrimitiveNameTranslator {
+
+    static private final String ERROR_TYPE = "!ERROR!";
+
+    public static String getName(final Primitive primitive) {
+
+        final String nickname;
+        if (primitive instanceof AuthenticationRequest) {
+            final AuthenticationRequest castedPrimitive = (AuthenticationRequest) primitive;
+            final String typeStr;
+            if (castedPrimitive.type == AuthenticationRequest.NEWLOGIN) {
+                typeStr = "NEWLOGIN";
+            }
+            else if (castedPrimitive.type == AuthenticationRequest.PREVIOUS) {
+                typeStr = "PREVIOUS";
+            }
+            else if (castedPrimitive.type == AuthenticationRequest.UNDEFINED) {
+                typeStr = "UNDEFINED";
+            }
+            else {
+                typeStr = ERROR_TYPE;
+            }
+            nickname = "AuthenticationRequest[type=" + typeStr + "]";
+        }
+        else if (primitive instanceof AuthenticationResponse) {
+            final AuthenticationResponse castedPrimitive = (AuthenticationResponse) primitive;
+            final String typeStr;
+            if (castedPrimitive.type == AuthenticationResponse.UNDEFINED) {
+                typeStr = "UNDEFINED";
+            }
+            else if (castedPrimitive.type == AuthenticationResponse.SUCCESS) {
+                typeStr = "SUCCESS";
+            }
+            else if (castedPrimitive.type == AuthenticationResponse.FAILED) {
+                typeStr = "FAILED";
+            }
+            else if (castedPrimitive.type == AuthenticationResponse.REQUEST) {
+                typeStr = "REQUEST";
+            }
+            else if (castedPrimitive.type == AuthenticationResponse.DATABASEERROR) {
+                typeStr = "DATABASEERROR";
+            }
+            else if (castedPrimitive.type == AuthenticationResponse.CLIENTALREADYLOGGED) {
+                typeStr = "CLIENTALREADYLOGGED";
+            }
+            else {
+                typeStr = ERROR_TYPE;
+            }
+            nickname = "AuthenticationResponse[type=" + typeStr + "]";
+        }
+        else if (primitive instanceof FormOperation) {
+            final FormOperation castedPrimitive = (FormOperation) primitive;
+            final String typeStr;
+            if(castedPrimitive.type == FormOperation.RESET) {
+                typeStr = "RESET";
+            }
+            else if(castedPrimitive.type == FormOperation.UPDATED_FORMS) {
+                typeStr = "UPDATED_FORMS";
+            }
+            else if(castedPrimitive.type == FormOperation.UPDATED_FORMS_COMPLETE) {
+                typeStr = "UPDATED_FORMS_COMPLETE";
+            }
+            else if(castedPrimitive.type == FormOperation.UPDATED_FORMS_CLEAR_FLAGS) {
+                typeStr = "UPDATED_FORMS_CLEAR_FLAGS";
+            }
+            else if(castedPrimitive.type == FormOperation.ALL_NEW_FORMS) {
+                typeStr = "ALL_NEW_FORMS";
+            }
+            else if(castedPrimitive.type == FormOperation.UPDATED_FORMS_PARTIAL) {
+                typeStr = "UPDATED_FORMS_PARTIAL";
+            }
+            else if(castedPrimitive.type == FormOperation.UPDATED_FORMS_CLEAR_RECENT_FLAGS) {
+                typeStr = "UPDATED_FORMS_CLEAR_RECENT_FLAGS";
+            }
+            else if(castedPrimitive.type == FormOperation.GET_STATUS) {
+                typeStr = "GET_STATUS";
+            }
+            else if(castedPrimitive.type == FormOperation.CLIENT_FAILURE) {
+                typeStr = "CLIENT_FAILURE";
+            }
+            else if(castedPrimitive.type == FormOperation.CLIENT_SUCCESS) {
+                typeStr = "CLIENT_SUCCESS";
+            }
+            else {
+                typeStr = ERROR_TYPE;
+            }
+            nickname = "FormOperation[type=" + typeStr + "]";
+        }
+        else {
+            nickname = "(not defined)";
+        }
+        return nickname;
+    }
+
+}// class
