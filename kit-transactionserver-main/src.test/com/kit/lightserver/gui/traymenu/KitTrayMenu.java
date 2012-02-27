496c444ee2a85715dcf393097184cb092b123f6d
Jose Flavio A <joseflavio at gmail dot com>
2012-02-27 13:54:16 +0100

    [dev] lancar o explorer em logs

--- kit-transactionserver-main/src.test/com/kit/lightserver/gui/traymenu/KitTrayMenu.java
+++ kit-transactionserver-main/src.test/com/kit/lightserver/gui/traymenu/KitTrayMenu.java
@@ -7,9 +7,14 @@ import java.awt.SystemTray;
 import java.awt.TrayIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.kit.lightserver.KITLightServerGui.KitTrayIconListeners;
 import com.kit.lightserver.config.ServerConfig;
@@ -18,51 +23,78 @@ import com.kit.lightserver.services.be.authentication.DatabaseConfig;
 
 public class KitTrayMenu {
 
+    static private final Logger LOGGER = LoggerFactory.getLogger(KitTrayMenu.class);
+
     private final TrayIcon trayIcon;
 
     public KitTrayMenu(final ServerConfig serverConfig, final DatabaseConfig dbConfig, final KitTrayIconListeners kitTrayIconListeners) {
 
-       if (!SystemTray.isSupported()) {
-           throw new RuntimeException("SystemTray is not supported");
-       }
+        if (!SystemTray.isSupported()) {
+            throw new RuntimeException("SystemTray is not supported");
+        }
 
-       ImageIcon imageIcon = ImageIconLoader.load(serverConfig.getServerIcon()) ;
+        ImageIcon imageIcon = ImageIconLoader.load(serverConfig.getServerIcon());
 
-       final PopupMenu popup = new PopupMenu();
-       MenuItem serverNameItem = new MenuItem(serverConfig.getServerName()+", porta "+serverConfig.getServerPort());
-       MenuItem databaseNameItem = new MenuItem(dbConfig.getDbName());
-       MenuItem shutDownItem = new MenuItem("Desligar o servidor");
+        PopupMenu popup = new PopupMenu();
+        MenuItem serverNameItem = new MenuItem(serverConfig.getServerName() + ", porta " + serverConfig.getServerPort());
+        MenuItem databaseNameItem = new MenuItem("Database: " + dbConfig.getDbName());
+        MenuItem goToLogsItem = new MenuItem("Ir para pasta de Logs");
+        MenuItem shutDownItem = new MenuItem("Desligar o servidor");
 
-       popup.add(serverNameItem);
-       popup.add(databaseNameItem);
-       popup.addSeparator();
-       popup.add(shutDownItem);
+        popup.add(serverNameItem);
+        popup.addSeparator();
+        popup.add(databaseNameItem);
+        popup.addSeparator();
+        popup.add(goToLogsItem);
+        popup.addSeparator();
+        popup.add(shutDownItem);
 
-       this.trayIcon = new TrayIcon(imageIcon.getImage());
-       trayIcon.setPopupMenu(popup);
-       trayIcon.setImageAutoSize(true);
+        this.trayIcon = new TrayIcon(imageIcon.getImage());
+        trayIcon.setPopupMenu(popup);
+        trayIcon.setImageAutoSize(true);
 
-       trayIcon.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(final ActionEvent e) {
-               JOptionPane.showMessageDialog(null, "Servidor KeepInTouch esta executando.");
-           }
-       });
+        trayIcon.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Servidor KeepInTouch esta executando.");
+            }
+        });
 
-       shutDownItem.addActionListener(new ActionListener() {
-           @Override
-           public void actionPerformed(final ActionEvent e) {
-               kitTrayIconListeners.desligarServidor();
-           }
-       });
+        goToLogsItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                KitTrayMenu.launchExplorerInLogsDirectory();
+            }
+        });
 
+        shutDownItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                kitTrayIconListeners.desligarServidor();
+            }
+        });
+
+    }
+
+    static protected void launchExplorerInLogsDirectory() {
+        File currentDir = new File("logs");
+        String currentFullDir = currentDir.getAbsolutePath();
+        ProcessBuilder processBuilder = new ProcessBuilder("explorer.exe", currentFullDir);
+        try {
+            processBuilder.start();
+        }
+        catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error tentando iniciar o explorer (verifique se o explorer.exe esta no seu PATH).");
+            LOGGER.error("Error launching explorer.exe", e);
+        }
     }
 
     public void install() {
         final SystemTray tray = SystemTray.getSystemTray();
         try {
             tray.add(trayIcon);
-        } catch (AWTException e) {
+        }
+        catch (AWTException e) {
             throw new RuntimeException("TrayIcon could not be added");
         }
     }
